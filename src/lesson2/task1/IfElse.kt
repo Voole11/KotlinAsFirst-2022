b65b59a7f7f55066fd871c8e21ae31827239807c
Voole <vladimir.oleynik2005@gmail.com>
2022-10-01 16:14:03 +0300

    commit (lesson 2)

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -69,8 +69,7 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
 fun ageDescription(age: Int): String = when {
-    age == 111 -> "$age лет"
-    age == 11 -> "$age лет"
+    age % 100 / 10 == 1 -> "$age лет"
     age % 10 == 1 -> "$age год"
     age in 5..20 -> "$age лет"
     age % 10 in 2..4 -> "$age года"
