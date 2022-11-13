c77f9a79286b37676b059fbd01c09f15a52f7a2c
Voole <vladimir.oleynik2005@gmail.com>
2022-11-13 19:13:40 +0300

    new commit

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -264,7 +264,55 @@ fun decimalFromString(str: String, base: Int): Int =
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    var num = n
+    var result = ""
+    while (num > 0) {
+        if (num >= 1000) {
+            result += "M"
+            num -= 1000
+        } else if (num >= 900) {
+            result += "CM"
+            num -= 900
+        } else if (num >= 500) {
+            result += "D"
+            num -= 500
+        } else if (num >= 400) {
+            result += "CD"
+            num -= 400
+        } else if (num >= 100) {
+            result += "C"
+            num -= 100
+        } else if (num >= 90) {
+            result += "XC"
+            num -= 90
+        } else if (num >= 50) {
+            result += "L"
+            num -= 50
+        } else if (num >= 40) {
+            result += "XL"
+            num -= 40
+        } else if (num >= 10) {
+            result += "X"
+            num -= 10
+        } else if (num >= 9) {
+            result += "IX"
+            num -= 9
+        } else if (num >= 5) {
+            result += "V"
+            num -= 5
+        } else if (num >= 4) {
+            result += "IV"
+            num -= 4
+        } else if (num >= 1) {
+            result += "I"
+            num -= 1
+        } else {
+            break
+        }
+    }
+    return result
+}
 
 /**
  * Очень сложная (7 баллов)
