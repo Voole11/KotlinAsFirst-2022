d54e18e2dd0bbf0da4105d317e6b49e6e2e9d45e
Voole <vladimir.oleynik2005@gmail.com>
2022-11-13 18:35:05 +0300

    new commit

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -140,7 +140,7 @@ fun mean(list: List<Double>): Double =
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double>  = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> = TODO()
 
 /**
  * Средняя (3 балла)
