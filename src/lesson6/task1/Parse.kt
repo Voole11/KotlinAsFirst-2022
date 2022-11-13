016099a0dfcd4a045ffaba0e726b71cf7ffb1950
Voole <vladimir.oleynik2005@gmail.com>
2022-11-13 21:25:20 +0300

    new commit

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -2,6 +2,8 @@
 
 package lesson6.task1
 
+import kotlin.math.max
+
 // Урок 6: разбор строк, исключения
 // Максимальное количество баллов = 13
 // Рекомендуемое количество баллов = 11
@@ -114,7 +116,16 @@ fun flattenPhoneNumber(phone: String): String = TODO()
  * Прочитать строку и вернуть максимальное присутствующее в ней число (717 в примере).
  * При нарушении формата входной строки или при отсутствии в ней чисел, вернуть -1.
  */
-fun bestLongJump(jumps: String): Int = TODO()
+fun bestLongJump(jumps: String): Int {
+    val split = jumps.split(" ")
+    var result = -1
+    for (i in split) {
+        if ((i !in "-%") && i.any { !it.isDigit() }) return -1
+        if (i.any { it.isDigit() })
+            result = max(result, i.toInt())
+    }
+    return result
+}
 
 /**
  * Сложная (6 баллов)
